aa22c2f68afe7ed7dbf3944123c5751666829496
MHendriF <hendrifebriansyah28@gmail.com>
2024-05-18 17:59:23 +0700

    add TextGenerateEffect

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import GridBackground from "./ui/GridBackground";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 export default function Hero() {
   return (
@@ -15,6 +16,17 @@ export default function Hero() {
         />
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
         <GridBackground />
+        <div className="flex justify-center relative my-20 z-10">
+          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-2-80">
+              Dynamic Web Magic with Next.js
+            </h2>
+            <TextGenerateEffect
+              className="text-center text-[40px] md:text-5xl lg:text-6xl"
+              words="Transforming Concepts into Seamless Web Experiences"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
