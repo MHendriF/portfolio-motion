878669c9cbe1bec7cb64c27d0c138d155657248e
MHendriF <hendrifebriansyah28@gmail.com>
2024-05-20 10:20:55 +0700

    add footer ui

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@
 import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
-import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -21,6 +21,7 @@ export default function Home() {
         <Clients />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
