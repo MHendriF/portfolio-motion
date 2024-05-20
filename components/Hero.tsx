3c0361549c5777408461a6eaa25c4c183fe3a0ca
MHendriF <hendrifebriansyah28@gmail.com>
2024-05-20 10:34:18 +0700

    bug fix ui

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 export default function Hero() {
   return (
-    <div className="tpb-30 pt-36">
+    <div className="pb-30 pt-30">
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
