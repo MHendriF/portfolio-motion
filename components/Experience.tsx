23667b476e60c1f71088742d16408dcd5ceac1ff
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-12 19:07:11 +0700

    update experience and footer

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -7,7 +7,7 @@ export default function Experience() {
   return (
     <div className="py-20" id="experience">
       <h1 className="heading">
-        My <span className="text-purple">work experience</span>
+        My <span className="text-purple-500 dark:text-purple-100">work experience</span>
       </h1>
       <div className="w-full mt-10 grid lg:grid-cols-4 grid-cols-1 gap-10">
         {workExperience.map((card) => (
@@ -18,20 +18,10 @@ export default function Experience() {
             containerClassName="flex-1 text-white border-neutral-200 dark:border-slate-800"
           >
             <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
-              <Image
-                src={card.thumbnail}
-                alt={card.thumbnail}
-                height={0}
-                width={0}
-                className="lg:w-32 md:w-20 w-16"
-              />
+              <Image src={card.thumbnail} alt={card.thumbnail} height={0} width={0} className="lg:w-32 md:w-20 w-16" />
               <div className="lg:ms-5">
-                <h1 className="text-start text-xl md:text-2xl font-bold">
-                  {card.title}
-                </h1>
-                <p className="text-start text-white-100 mt-3 font-semibold">
-                  {card.desc}
-                </p>
+                <h1 className="text-start text-xl md:text-2xl font-bold">{card.title}</h1>
+                <p className="text-start text-white-100 mt-3 font-semibold">{card.desc}</p>
               </div>
             </div>
           </Button>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,41 +9,36 @@ export default function Footer() {
     <footer className="w-full pb-10 mb-[100px] md:mb-5" id="contact">
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
-          Ready to take <span className="text-purple">your</span> digital
-          presence to the next level?
+          Ready to take <span className="text-purple-500 dark:text-purple-100">your</span> digital presence to the next
+          level?
         </h1>
-        <p className="text-white-200 md:mt-10 my-5 text-center">
-          Reach out to me today and let&apos;s discuss how I can help you
-          achieve your goals.
+        <p className="text-slate-950 dark:text-white-200 md:mt-10 my-5 text-center">
+          Reach out to me today and let&apos;s discuss how I can help you achieve your goals.
         </p>
         <a href="mailto:hendrifebriansyah28@gmail.com">
-          <MagicButton
-            title="Get in touch"
-            icon={<FaLocationArrow />}
-            position="right"
-          />
+          <MagicButton title="Get in touch" icon={<FaLocationArrow />} position="right" />
         </a>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright &copy; 2024 MHF
-        </p>
+        <p className="md:text-base text-sm md:font-normal font-light">Copyright &copy; 2024 MHF</p>
         <div className="flex items-center md:gap-3 gap-6">
-          {socialMedia.map(({ id, img }) => (
+          {socialMedia.map(({ id, img, link }) => (
             <div
               key={id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-100 dark:bg-black-200 rounded-lg border border-black-300"
             >
-              <Image
-                src={img}
-                alt="social-media"
-                width={20}
-                height={20}
-                style={{
-                  width: "auto",
-                  height: "auto",
-                }}
-              />
+              <a href={link}>
+                <Image
+                  src={img}
+                  alt="social-media"
+                  width={20}
+                  height={20}
+                  style={{
+                    width: "auto",
+                    height: "auto",
+                  }}
+                />
+              </a>
             </div>
           ))}
         </div>
